6632b4a150beac768bcaa89af18e8857d079b951
AlexanderHuallpa2024 <alehuallpah@upt.pe>
2024-11-03 18:22:07 -0500

    DashboardLayout - Diseño

--- src/ReactGPT.tsx
+++ src/ReactGPT.tsx
@@ -1,5 +1,8 @@
+import { RouterProvider } from "react-router-dom"
+import { router } from "./presentation/router/router"
+
 export const ReactGPT = () => {
   return (
-    <h1>ReactGPT</h1>
+    <RouterProvider router={ router}/>
   )
 }
